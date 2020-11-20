3bdcb3216f7ff44c5cbd5493216529f25af90778
lazzy07 <222lasantha@gmail.com>
2020-11-20 19:20:01 +0530

    Pixi resize problem fixed

--- src/components/ProjectPreviewComponent.tsx
+++ src/components/ProjectPreviewComponent.tsx
@@ -2,23 +2,29 @@ import React, { createRef, useEffect } from 'react'
 import { defaultColors } from '../constants/Colors';
 import { ICON_TYPES } from '../interfaces/IconTypes';
 import IconComponent from './IconComponent';
-import Girl01 from "../img/projects/girl01.png";
+import Girl01 from "../img/projects/girl/girl01.png";
+import Girl01Depth from "../img/projects/girl/girl01_disp.jpg";
 import Button from './Button';
+import { Application, filters, Sprite } from "pixi.js";
+
 interface Props {
 
 }
+
+const depthMap = Sprite.from(Girl01Depth);
+const displaceFilter = new filters.DisplacementFilter(depthMap);
 
 export default function ProjectPreviewComponent(props: Props) {
   const containerRef = createRef<HTMLDivElement>();
   const cardRef = createRef<HTMLDivElement>();
   const imageRef = createRef<HTMLDivElement>();
-  const titleRef = createRef<HTMLDivElement>();
-  const descRef = createRef<HTMLDivElement>();
-  const iconContainerRef = createRef<HTMLDivElement>();
-  const buttonRef = createRef<HTMLDivElement>();
+  const rendererRef = createRef<HTMLDivElement>();
+  const img = Sprite.from(Girl01);
+
+  const imgRes = img.width / img.height;
 
   const renderIcon = (icon: ICON_TYPES) => {
-    return <div key={icon} className="col-xs-2">
+    return <div key={icon} className="col-xs-3">
       <IconComponent
         containerStyles={{ padding: 10 }}
         animations
@@ -47,32 +53,27 @@ export default function ProjectPreviewComponent(props: Props) {
       const xAxis = (window.innerWidth / 2 - e.pageX) / effectPowerX;
       const yAxis = (window.innerHeight / 2 - e.pageY) / effectPowerY;
 
+      if (displaceFilter) {
+        displaceFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 100;
+        displaceFilter.scale.y = (window.innerWidth / 2 - e.clientY) / 100;
+      }
+
       if (cref) {
         cref.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
       }
     }
 
-    const onMouseEnter = (e: MouseEvent) => {
+    const onMouseEnter = (_: MouseEvent) => {
       const cref = cardRef.current;
       if (cref)
         cref.style.transition = "none";
-
-      //popouts
-      if (imageRef.current) {
-        imageRef.current.style.transform = "translateZ(150px)"
-      }
     }
 
-    const onMouseLeave = (e: MouseEvent) => {
+    const onMouseLeave = (_: MouseEvent) => {
       const cref = cardRef.current;
       if (cref) {
         cref.style.transition = "all 0.5s ease";
         cref.style.transform = `rotateY(0deg) rotateX(0deg)`;
-
-        //popouts
-        if (titleRef.current) {
-          titleRef.current.style.transform = "translateZ(0px)"
-        }
       }
     }
 
@@ -88,9 +89,37 @@ export default function ProjectPreviewComponent(props: Props) {
         ref.addEventListener("mouseleave", onMouseLeave);
         ref.addEventListener("mouseenter", onMouseEnter);
       }
-
     }
-  }, [containerRef, cardRef, imageRef])
+  }, [containerRef, cardRef, imageRef]);
+
+  useEffect(() => {
+    const app = new Application({ width: rendererRef.current!.clientWidth, height: rendererRef.current!.clientHeight });
+    rendererRef.current?.appendChild(app.renderer.view);
+
+    app.stage.addChild(depthMap);
+
+    displaceFilter.scale.x = 0;
+    displaceFilter.scale.y = 0;
+    img.height = app.renderer.height;
+    img.width = img.height * imgRes;
+
+
+
+    app.stage.filters = [displaceFilter];
+    app.stage.addChild(img);
+
+    const onResizeRenderer = (_: UIEvent) => {
+      setTimeout(() => {
+        app.renderer.resize(rendererRef.current!.clientWidth, imageRef.current!.clientHeight)
+      }, 500)
+    }
+
+    window.addEventListener("resize", onResizeRenderer)
+
+    return () => {
+      window.removeEventListener("resize", onResizeRenderer);
+    }
+  }, [rendererRef, img, imageRef, imgRes])
 
   return (
     <div ref={containerRef} className="container" style={{
@@ -98,35 +127,31 @@ export default function ProjectPreviewComponent(props: Props) {
     }}>
       <div
         ref={cardRef}
-        className="col-md-8 col-sm-10 col-sm-12 col-md-offset-2"
+        className="col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1"
         style={{
-          minHeight: 400,
           filter: "drop-shadow( 9px 9px 8px rgba(0, 0, 0, .4))",
           backgroundColor: defaultColors.DEFAULT_FONT_COLOR,
           borderRadius: "80px 20px 80px 20px",
           overflow: "hidden"
         }}>
-        <div className="row" style={{ zIndex: 2, position: "relative" }}>
-          <div ref={imageRef} className="col-sm-6 col-xs-12" style={{ overflow: "hidden", padding: 0, marginBottom: -4 }}>
-            <img src={Girl01} alt="" width="100%" />
+        <div className="row" style={{ zIndex: 2, position: "relative", transformStyle: "preserve-3d", perspective: "1000px" }}>
+          <div ref={imageRef} className="col-sm-6 col-xs-12" style={{ overflow: "hidden", padding: 0, marginBottom: -4, zIndex: 1 }}>
+            {/* <img src={Girl01} alt="" width="100%" /> */}
+            <div ref={rendererRef} style={{ width: "100%", height: 480, backgroundColor: "khaki" }}></div>
           </div>
-          <div className="col-sm-6 col-xs-12" style={{ padding: 50, transformStyle: "preserve-3d" }}>
-            <div ref={titleRef}>
-              <h1
-                style={{ color: defaultColors.DEFAULT_BACKGROUND_COLOR, fontWeight: "bolder", fontSize: "2.1rem" }}
-              >
-                Girl Protrait
-              </h1>
-            </div>
-            <div ref={descRef}>
-              <p style={{ paddingTop: 10, paddingBottom: 20, color: defaultColors.SECONDARY_FONT_COLOR }}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur laudantium dicta est repudiandae alias at! Obcaecati voluptatum nihil ad incidunt ullam praesentium laudantium accusantium consequatur hic cumque debitis, blanditiis magni!
-              </p>
-            </div>
-            <div ref={iconContainerRef} className="row">
+          <div className="col-sm-6 col-xs-12" style={{ padding: 50, zIndex: 2 }}>
+            <h1
+              style={{ color: defaultColors.DEFAULT_BACKGROUND_COLOR, fontWeight: "bolder", fontSize: "2.1rem", filter: `drop-shadow(3px 3px 2px ${defaultColors.DEFAULT_BACKGROUND_COLOR}45` }}
+            >
+              Girl Protrait
+            </h1>
+            <p style={{ paddingTop: 10, paddingBottom: 20, color: defaultColors.SECONDARY_FONT_COLOR }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur laudantium dicta est repudiandae alias at! Obcaecati voluptatum nihil ad incidunt ullam praesentium laudantium
+            </p>
+            <div className="row">
               {renderIcons(["blender", "substance designer", "substance painter"])}
             </div>
-            <div ref={buttonRef} style={{ paddingTop: 30 }}>
+            <div style={{ paddingTop: 30 }}>
               <Button />
             </div>
           </div>
